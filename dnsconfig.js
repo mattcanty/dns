a726a1bd2e80da4f0484cbe8a4404bb46ac5195b
Matt Canty <matt.canty@tandem.co.uk>
2017-05-05 16:11:27 +0100

    Add matthewcanty.co.uk and katybrody.co.uk

--- dnsconfig.js
+++ dnsconfig.js
@@ -6,3 +6,13 @@ D("mattcanty.com", NON_REG, DnsProvider(R53),
     A("lifedex","151.101.65.195"),
     TXT("@","google-site-verification=qbRypdAcGwptTI43UaQrfDvAEN7-DkALAcnME-5Kwn0")
 );
+
+D("matthewcanty.co.uk", NON_REG, DnsProvider(R53),
+    CNAME("www","dns.strikingly.com."),
+    CNAME("weatherclock","analog-weather-clock.herokuapp.com.")
+);
+
+D("katybrody.co.uk", NON_REG, DnsProvider(R53),
+    A("www","88.208.252.9"),
+    A("@","88.208.252.9")
+);
