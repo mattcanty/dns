a5971717f8ea24351f4a1a56c28ac805fc6f84b8
Matt Canty <matt.canty@tandem.co.uk>
2017-10-06 16:09:54 +0100

    Playing with syntax

--- dnsconfig.js
+++ dnsconfig.js
@@ -4,7 +4,7 @@ var R53 = NewDnsProvider("r53", "ROUTE53");
 D("mattcanty.com", NON_REG, DnsProvider(R53),
     A("lifedex","151.101.1.195"),
     A("lifedex","151.101.65.195"),
-    CNAME("chords.mattcanty.com","mattcanty.github.io"),
+    CNAME("chords","mattcanty.github.io."),
     TXT("@","google-site-verification=qbRypdAcGwptTI43UaQrfDvAEN7-DkALAcnME-5Kwn0")
 );
 
