6dedd97c5052db4a5ff02f880b6c396581845535
Matt Canty <matt.canty@tandem.co.uk>
2017-10-06 16:19:26 +0100

    Match configuration to existing R53

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,12 +5,15 @@ D("mattcanty.com", NON_REG, DnsProvider(R53),
     A("lifedex","151.101.1.195"),
     A("lifedex","151.101.65.195"),
     CNAME("chords","mattcanty.github.io."),
+    CNAME("c37trtl6ubra6yp4enjsp4tegjqn3lt7._domainkey","c37trtl6ubra6yp4enjsp4tegjqn3lt7.dkim.amazonses.com."),
+    CNAME("debf6om2flnqorv7k5ncutk7yzlutu7t._domainkey","debf6om2flnqorv7k5ncutk7yzlutu7t.dkim.amazonses.com."),
+    CNAME("odcgmdy25sn67hci6ko6xmn5hccsgxvb._domainkey","odcgmdy25sn67hci6ko6xmn5hccsgxvb.dkim.amazonses.com."),
+    TXT("_amazonses","i0uXmZmm8Ce/k2jwpEv+Y9ZXmxHJkNWz8MBUGsjcCDU="),
     TXT("@","google-site-verification=qbRypdAcGwptTI43UaQrfDvAEN7-DkALAcnME-5Kwn0")
 );
 
 D("matthewcanty.co.uk", NON_REG, DnsProvider(R53),
-    CNAME("www","dns.strikingly.com."),
-    CNAME("weatherclock","analog-weather-clock.herokuapp.com.")
+    CNAME("www","dns.strikingly.com.")
 );
 
 D("katybrody.co.uk", NON_REG, DnsProvider(R53),
