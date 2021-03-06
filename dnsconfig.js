7ac1354160ea680a2e6bbec22dc6a7944c3c11e9
Matt Canty <matthewcanty@gmail.com>
2021-03-06 21:55:59 +0000

    revert

--- dnsconfig.js
+++ dnsconfig.js
@@ -16,8 +16,7 @@ D("mattcanty.com", NON_REG, DnsProvider(R53),
     CNAME("blog", "gracious-varahamihira-35ff13.netlify.com."),
     R53_ALIAS("rainclock", "A", "d2le1566l2fx93.cloudfront.net", R53_ZONE('Z2FDTNDATAQYW2')),
     CNAME("_c0e6e8fc4682262e6c452a1b1d43d327", "_2b82288279a845962a0b4c2854d6a418.tljzshvwok.acm-validations.aws."),
-    CNAME("bass", "angry-yalow-e2f303.netlify.app."),
-    CNAME("_c0e6e8fc4682262e6c452a1b1d43d327.mattcanty.com.", "_2b82288279a845962a0b4c2854d6a418.tljzshvwok.acm-validations.aws.")
+    CNAME("bass", "angry-yalow-e2f303.netlify.app.")
 );
 
 D("katybrody.co.uk", NON_REG, DnsProvider(R53),
