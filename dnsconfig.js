7d3009dc3272b41a8d99f06c9e36856ac2aef7fc
Matt Canty <matthewcanty@gmail.com>
2021-03-07 11:30:20 +0000

    Add dot to rainclock

--- dnsconfig.js
+++ dnsconfig.js
@@ -14,7 +14,7 @@ D("mattcanty.com", NON_REG, DnsProvider(R53),
     TXT("@", "google-site-verification=qbRypdAcGwptTI43UaQrfDvAEN7-DkALAcnME-5Kwn0"),
     TXT("@", "keybase-site-verification=A7ns5-QzuyAi-fb-B5h2gmgnU9IvT9KNMZH24hksx54"),
     CNAME("blog", "gracious-varahamihira-35ff13.netlify.com."),
-    R53_ALIAS("rainclock", "A", "d2le1566l2fx93.cloudfront.net", R53_ZONE('Z2FDTNDATAQYW2')),
+    R53_ALIAS("rainclock", "A", "d2le1566l2fx93.cloudfront.net.", R53_ZONE('Z2FDTNDATAQYW2')),
     CNAME("_c0e6e8fc4682262e6c452a1b1d43d327", "_2b82288279a845962a0b4c2854d6a418.tljzshvwok.acm-validations.aws."),
     CNAME("bass", "angry-yalow-e2f303.netlify.app."),
     R53_ALIAS("photo", "A", "d-k6skdb15gj.execute-api.eu-west-2.amazonaws.com.", R53_ZONE('ZJ5UAJN8Y3Z2Q'))
