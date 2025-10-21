32d78347f0a03401d26a71e9688c01c91e2e3180
Matt Canty <matthewcanty@gmail.com>
2025-10-21 15:13:21 +0100

    Add 3d.controlaltrepeat.net

--- dnsconfig.js
+++ dnsconfig.js
@@ -45,7 +45,8 @@ D("controlaltrepeat.net", NON_REG, DnsProvider(R53),
     CNAME("rmfzhb3soumbxtasa3zasa2rv4atm7ec._domainkey", "rmfzhb3soumbxtasa3zasa2rv4atm7ec.dkim.custom-email-domain.stripe.com."),
     CNAME("bounce", "custom-email-domain.stripe.com."),
     TXT("@", "stripe-verification=4997d4f786aa534f5c07ff109736edb2a723e60d4eddce354359a0582e9462f3"),
-    TXT("_dmarc", "v=DMARC1; p=none; rua=mailto:postmaster@controlaltrepeat.net")
+    TXT("_dmarc", "v=DMARC1; p=none; rua=mailto:postmaster@controlaltrepeat.net"),
+    CNAME("3d", "custom-3d-prints.netlify.app."),
 );
 
 D("bassguitarbook.com", NON_REG, DnsProvider(R53),
